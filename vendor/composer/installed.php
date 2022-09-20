9b3cd2cf34cf0939b22595ba33bfd01dc4eb814c
Kyle Davis <kdaviswx@ou.edu>
2022-09-20 15:17:21 -0500

    Added composer and kint example.

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'dcbe9e30e93d4e052f79cfbe16978dbec7650253',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'dcbe9e30e93d4e052f79cfbe16978dbec7650253',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'raveren/kint' => array(
+            'pretty_version' => 'v1.2.10',
+            'version' => '1.2.10.0',
+            'reference' => '16b6ed64d7c3dfd215f44497f98fb1d49cdbdd43',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../raveren/kint',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
